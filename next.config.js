4e6312bbbaf0bdde259923cba4912c5fff37b9ae
kyoungholee <dlrudgh1303@gmail.com>
2023-11-21 22:29:16 +0900

    .env 파일을 이용해 서버 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:3001/api/:path*', // Express 서버 주소
+        },
+      ];
+    },
+  };
+  
